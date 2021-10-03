b1a8c58c8bc21ea5ca972270860dc669c0f8affd
Astroth1984 <elmounir.othmane666@gmail.com>
2021-10-03 03:03:19 +0200

    add Simplified View

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,12 +4,13 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
-    console.log(data);
+    const { data, isFetching } = useGetCryptosQuery(10);
+    // console.log(data);
     const globalStats = data?.data?.stats;
 
     if (isFetching) return 'Loading...'
@@ -25,6 +26,18 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
+            {/* Show 10 Cryptocurrencies using the variable simplified on the Cryptocurrencies Componenet */}
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+            {/* Show  Cryptocurrencies news using the variable simplified on the News Componenet */}
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
