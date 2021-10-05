26042fec2d7753aab3f72773cc03e769ea29c4a9
Astroth1984 <elmounir.othmane666@gmail.com>
2021-10-05 22:23:53 +0200

    Loader Component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,6 +6,8 @@ import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
+import Loader from './Loader';
+
 const { Text, Title } = Typography;
 const { Option } = Select;
 
@@ -16,7 +18,7 @@ const News = ({ simplified }) => {
     const { data } = useGetCryptosQuery(100);
     const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
     console.log(cryptoNews)
-    if (!cryptoNews?.value) return 'Loading ...';
+    if (!cryptoNews?.value) return <Loader />;
     return (
         <Row gutter={[24, 24]}>
             {!simplified && (
