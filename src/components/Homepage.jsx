907b8ab33c8dabba800d029c5348529c70a4701d
Astroth1984 <elmounir.othmane666@gmail.com>
2021-10-05 22:22:29 +0200

    use Loader component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Cryptocurrencies, News } from '../components';
 
+import Loader from './Loader';
+
 const { Title } = Typography;
 
 const Homepage = () => {
@@ -13,7 +15,7 @@ const Homepage = () => {
     // console.log(data);
     const globalStats = data?.data?.stats;
 
-    if (isFetching) return 'Loading...'
+    if (isFetching) return <Loader />;
 
     return (
         <>
